896a0d9caf2e90ca8b10e99c22e9000e91420370
scottwan <scottwan@foxmail.com>
2021-11-19 23:47:05 +0800

    comment ads129xr driver config

--- drivers/zephyr/sensor/ads129xr/ads129xr.c
+++ drivers/zephyr/sensor/ads129xr/ads129xr.c
@@ -18,10 +18,10 @@
 #define DT_DRV_COMPAT ti_ads129xr
 
 struct ads129xr_config {
-    const uint8_t start_pin;    //开始AD转换
-	const uint8_t ready_pin;    //AD转换数据准备好
-    const uint8_t reset_pin;    //重启
-    const uint8_t pwd_pin;      //关闭电源
+    const uint8_t start_pin;    //开始AD转换，高电平有效
+	const uint8_t ready_pin;    //AD转换数据准备好，低电平有效
+    const uint8_t reset_pin;    //寄存器重置，低电平有效
+    const uint8_t pwd_pin;      //关闭电源，低电平有效
 };
 
 struct ads129xr_data {
